312f05950f6573737b9101b174719b17614ec8c2
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2022-01-28 12:25:14 +0630

    refactor: split to multiple components

--- src/index.js
+++ src/index.js
@@ -1,99 +1,11 @@
-import calculateWinner from './helpers/calculateWinner' ;
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import "./index.css";
 
-const Square = ({value, onClick}) => {
-  return (
-    <button 
-      className="square" 
-      onClick={onClick}
-    >
-      {value}
-    </button>
-  );
-};
-
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-  
-  handleClick = (i) => {
-    const squares = [...this.state.squares];
-    if (squares[i] || calculateWinner(this.state.squares)) {
-      return
-    };
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      squares,
-      xIsNext: !this.state.xIsNext
-    });
-  };
-
-  renderSquare(i) {
-    return (
-      <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    console.log(winner);
-    const status = winner ? 
-                    `Winner : ${winner}` 
-                    :`Next player: ${this.state.xIsNext ? "X" : "O"}`;
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
 ReactDOM.render(
-  <Game />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
