754825143804c4320e971f69a5040499eb2d5f72
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2022-02-11 11:39:34 +0630

    feat: add scores for each game move

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Board from './Board';
 import calculateWinner from '../helpers/calculateWinner';
 
 const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
-  const squares = history[history.length - 1];
-  const winner = calculateWinner(squares);
-  const isDraw = !winner && !squares.includes(null);
-  const gameStatus = winner ? `${squares[winner[0]]} Win!` 
-                    : isDraw ? "Draw!"
-                    : xIsNext ? "X Turn" 
-                    : "O Turn";
+  const [scores, setScores] = useState({
+    playerX: 0, playerY: 0, tie: 0
+  });
 
   const handleClick = (i) => {
     const newHistory = [...history];
@@ -23,27 +19,52 @@ const Game = () => {
     setXIsNext(!xIsNext);
   }
 
+  const squares = history[history.length - 1];
+  const win = calculateWinner(squares);
+  const winner = win && squares[win[0]];
+  const isDraw = !win && !squares.includes(null);
+  const gameStatus = win ? `${winner} Win!` 
+                    : isDraw ? "Draw!"
+                    : xIsNext ? "X Turn" 
+                    : "O Turn";
+
+  useEffect(() => {
+    let {playerX, playerY, tie} = {...scores};
+    if (winner) {
+      winner === "X" ? playerX++ : playerY++;
+      const newScores = {playerX, playerY, tie};
+      setScores({...newScores});
+    } 
+    if (isDraw) {
+      tie++;
+      const newScores = {...scores, tie};
+      setScores({...newScores});
+    }
+  }, [xIsNext]);
+
+  const {playerX, playerY, tie} = scores;
+
   return (
     <div className='game w-100 h-100 p-1'>
-      <h1 className='game__status'>{gameStatus}</h1>
+      <h1 id='game_status' className='game__status'>{gameStatus}</h1>
       <Board 
         squares={squares} 
-        winner={winner} 
+        winner={win} 
         isDraw={isDraw} 
         onClick={handleClick}
       />
       <div className="game__scores">
         <div className="game__scores--item">
           <span>Player (X)</span>
-          <span>0</span>
+          <span>{playerX}</span>
         </div>
         <div className="game__scores--item">
           <span>Tie</span>
-          <span>2</span>
+          <span>{tie}</span>
         </div>
         <div className="game__scores--item">
           <span>Player (O)</span>
-          <span>3</span>
+          <span>{playerY}</span>
         </div>
       </div>
     </div>
